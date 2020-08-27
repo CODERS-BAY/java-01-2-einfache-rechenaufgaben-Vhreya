d46ebf7b0ed7a75852fe82419ac9b5c7141679e5
Vanessa <va.au@gmx.at>
2020-08-27 12:29:53 +0200

    revised

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.sql.SQLOutput;
 import java.text.DecimalFormat;
 
@@ -7,21 +8,19 @@ public class Calculations {
         int firstNumber = 165;
         int secondNumber= 115;
 
-        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
-        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
-        System.out.println(firstNumber + " * " + secondNumber + " = " + firstNumber * secondNumber);
-        System.out.println(firstNumber + " / " + secondNumber + " = " + firstNumber / secondNumber);
-        System.out.println();
+        System.out.printf("%d + %d = %8d%n", firstNumber, secondNumber, firstNumber + secondNumber);
+        System.out.printf("%d - %d = %8d%n", firstNumber, secondNumber, firstNumber - secondNumber);
+        System.out.printf("%d * %d = %8d%n", firstNumber, secondNumber, firstNumber * secondNumber);
+        System.out.printf("%d / %d = %8d%n%n", firstNumber, secondNumber, firstNumber / secondNumber);
 
 
         double firstNumberDouble = 165;
         double secondNumberDouble= 115;
 
-        System.out.println(firstNumberDouble + " + " + secondNumberDouble + " = " + (firstNumberDouble + secondNumberDouble));
-        System.out.println(firstNumberDouble + " - " + secondNumberDouble + " = " + (firstNumberDouble - secondNumberDouble));
-        System.out.println(firstNumberDouble + " * " + secondNumberDouble + " = " + firstNumberDouble * secondNumberDouble);
-        System.out.println(firstNumberDouble + " / " + secondNumberDouble + " = " + firstNumberDouble / secondNumberDouble);
+        System.out.printf("%.2f + %.2f = %8.2f%n", firstNumberDouble, secondNumberDouble, firstNumberDouble + secondNumberDouble);
+        System.out.printf("%.2f - %.2f = %8.2f%n", firstNumberDouble, secondNumberDouble, firstNumberDouble - secondNumberDouble);
+        System.out.printf("%.2f * %.2f = %8.2f%n", firstNumberDouble, secondNumberDouble, firstNumberDouble * secondNumberDouble);
+        System.out.printf("%.2f / %.2f = %8.2f%n", firstNumberDouble, secondNumberDouble, firstNumberDouble / secondNumberDouble);
 
-        //Wie kann ich die Dezimalstellen begrenzen?
     }
 }
